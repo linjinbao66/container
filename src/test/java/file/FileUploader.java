86138db0092bad1bbfea94f44edec727a2637992
linjinbao66 <linjinbao66@gmail.com>
2020-07-05 03:36:30 +0800

    集成MinIO文件系统

--- src/test/java/file/FileUploader.java
+++ src/test/java/file/FileUploader.java
@@ -5,33 +5,35 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.security.NoSuchAlgorithmException;
 import java.security.InvalidKeyException;
+import java.util.Iterator;
 import java.util.List;
 
 
-import io.minio.MinioClient;
-import io.minio.ObjectWriteResponse;
-import io.minio.PutObjectArgs;
-import io.minio.UploadObjectArgs;
+import io.minio.*;
 import io.minio.errors.*;
+import io.minio.http.Method;
 import io.minio.messages.Bucket;
+import io.minio.messages.Item;
 
 public class FileUploader {
-    public static void main(String[] args) throws IOException, InvalidKeyException, InvalidResponseException, InsufficientDataException, NoSuchAlgorithmException, ServerException, InternalException, XmlParserException, InvalidBucketNameException, ErrorResponseException {
+    public static void main(String[] args) throws Exception {
 
         MinioClient client = MinioClient.builder()
                 .endpoint("http://192.168.43.128:30022")
                 .credentials("minioadmin","minioadmin")
                 .build();
 
-        String fileName = "D:\\文件备份\\数据治理--库表接口文档2.txt";
-
-        ObjectWriteResponse demo1 = client.uploadObject(UploadObjectArgs.builder()
+        Iterable<Result<Item>> demo1 = client.listObjects(ListObjectsArgs.builder()
                 .bucket("demo1")
-                .object("数据治理.txt")
-                .filename(fileName)
                 .build()
         );
-        String s = demo1.versionId();
-        System.out.println(s);
+
+        for (Iterator iter = demo1.iterator(); iter.hasNext();){
+            Result result = (Result) iter.next();
+            System.out.println(result.get());
+        }
+
+
     }
 }
+
